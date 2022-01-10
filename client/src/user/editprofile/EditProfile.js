5331e6e9fcb3f37d2ec7849c6e33718c1100a244
shmiitg <shubhamank002@gmail.com>
2022-01-10 12:37:28 +0530

    user page responsiveness added

--- client/src/user/editprofile/EditProfile.js
+++ client/src/user/editprofile/EditProfile.js
@@ -44,53 +44,47 @@ const EditProfile = () => {
 
     if (loading) return (<Loading />)
     return (
-        <div className="container">
-            <div className={classes["profile-edit-container"]}>
-                <div className={classes["profile-edit-card"]}>
-                    <div className={classes["profile-header"]}>
-                        <h1>Personal Information</h1>
-                        <div className={classes["edit-btn"]}>
-                            <button onClick={saveDetail}>Update</button>
-                        </div>
+        <div className={classes["profile-edit-container"]}>
+            <div className={classes["profile-edit-card"]}>
+                <div className={classes["profile-header"]}>
+                    <h1>Personal Information</h1>
+                    <div className={classes["edit-btn"]}>
+                        <button onClick={saveDetail}>Update</button>
                     </div>
-                    <div className={classes["col"]}>
-                        <div className={classes["type-1"]}>
-                            <div className={classes["col-placeholder"]}>name</div>
-                            <input type="text" placeholder='Name' name="name" onChange={handleInput} value={user.name} />
-                        </div>
-                        <div className={classes["type-1"]}>
-                            <div className={classes["col-placeholder"]}>education</div>
-                            <input type="text" placeholder='Education' name="education" onChange={handleInput} value={user.education} />
-                        </div>
+                </div>
+                <div className={classes["col"]}>
+                    <div className={classes["type-1"]}>
+                        <div className={classes["col-placeholder"]}>name</div>
+                        <input type="text" placeholder='Name' name="name" onChange={handleInput} value={user.name} />
                     </div>
-                    <div className={classes["col"]}>
-                        <div className={classes["type-1"]}>
-                            <div className={classes["col-placeholder"]}>gender</div>
-                            <select name="gender" onChange={handleInput}>
-                                <option value="" selected={user.country === 'Your gender'}>--Your Gender--</option>
-                                <option value="Male" selected={user.gender === 'Male'}>Male</option>
-                                <option value="Female" selected={user.gender === 'Female'}>Female</option>
-                            </select>
-                        </div>
-                        <div className={classes["type-1"]}>
-                            <div className={classes["col-placeholder"]}>birthday</div>
-                            <input type="date" placeholder='Select date' name="birthday" onChange={handleInput} value={user.birthday} />
-                        </div>
+                    <div className={classes["type-1"]}>
+                        <div className={classes["col-placeholder"]}>education</div>
+                        <input type="text" placeholder='Education' name="education" onChange={handleInput} value={user.education} />
                     </div>
-                    <div className={classes["col"]}>
-                        <div className={classes["type-1"]}>
-                            <div className={classes["col-placeholder"]}>location</div>
-                            <select name="country" onChange={handleInput}>
-                                <option value="" selected={user.country === 'Your location'}>--Select Location--</option>
-                                {countries.map((location, index) => (
-                                    <option key={index} value={location} selected={user.location === location}>{location}</option>
-                                ))}
-                            </select>
-                        </div>
+                    <div className={classes["type-1"]}>
+                        <div className={classes["col-placeholder"]}>gender</div>
+                        <select name="gender" onChange={handleInput}>
+                            <option value="" selected={user.country === 'Your gender'}>--Your Gender--</option>
+                            <option value="Male" selected={user.gender === 'Male'}>Male</option>
+                            <option value="Female" selected={user.gender === 'Female'}>Female</option>
+                        </select>
+                    </div>
+                    <div className={classes["type-1"]}>
+                        <div className={classes["col-placeholder"]}>birthday</div>
+                        <input type="date" placeholder='Select date' name="birthday" onChange={handleInput} value={user.birthday} />
+                    </div>
+                    <div className={classes["type-1"]}>
+                        <div className={classes["col-placeholder"]}>location</div>
+                        <select name="country" onChange={handleInput}>
+                            <option value="" selected={user.country === 'Your location'}>--Select Location--</option>
+                            {countries.map((location, index) => (
+                                <option key={index} value={location} selected={user.location === location}>{location}</option>
+                            ))}
+                        </select>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
